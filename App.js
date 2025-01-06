b3393cd36cecb279b449ee70aa744c04d7e5c2c3
LCB2021029-Badri <lcb2021029@iiitl.ac.in>
2025-01-06 19:09:10 +0530

    Final basic app with  instructions

--- App.js
+++ App.js
@@ -82,7 +82,7 @@ function App() {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next:
+          Ok Badri
           </Section>
           <LearnMoreLinks />
         </View>
